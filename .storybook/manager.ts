308592c3a35b786458255093c07dd160ad351c67
xing_lin <xing_lin@fzzixun.com>
2025-10-12 22:25:14 +0800

    docs: update readme.md

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,12 +1,13 @@
 import { addons } from 'storybook/manager-api';
 import { create } from 'storybook/internal/theming';
-import { version } from '../package.json';
 
 const theme = create({
   base: 'light',
   brandUrl: 'https://www.npmjs.com/package/browseract-ui',
   brandTarget: '_blank',
-  brandTitle: `BrowserAct UI: V${version}`,
+  brandTitle: `BrowserAct UI`,
+  brandImage:
+    'https://img.shields.io/npm/v/browseract-ui.svg?label=browseract-ui',
 });
 
 addons.setConfig({
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # BrowserAct UI Components
 
 [![NPM version](https://img.shields.io/npm/v/browseract-ui.svg?style=flat)](https://www.npmjs.com/package/browseract-ui)
+[![Coverage](https://coveralls.io/repos/github/xing-lin/browseract-ui/badge.svg?branch=main)](https://coveralls.io/github/xing-lin/browseract-ui?branch=main)
 
 ## Installation
 
