0a62bf4e1c49fa29c03ee2354e199c517c797aeb
xing_lin <xing_lin@fzzixun.com>
2025-10-13 22:16:47 +0800

    feat: update export icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,14 @@
 export { IconCopy } from './icons/copy/IconCopy';
-
-export {
-  IconDislike,
-  type IconDislikeProps,
-} from './icons/dislike/IconDislike';
-
+export { IconDislike, type IconDislikeProps } from './icons/dislike/IconDislike';
 export { IconLike, type IconLikeProps } from './icons/like/IconLike';
 
-export {
-  IconMenuAgent,
-  type IconMenuAgentProps,
-} from './icons/menus/agent/IconMenuAgent';
+// menus
+export { IconMenuAgent, type IconMenuAgentProps } from './icons/menus/agent/IconMenuAgent';
+export { IconMenuCredential, type IconMenuCredentialProps } from './icons/menus/credential/IconMenuCredential';
+export { IconMenuDashboard, type IconMenuDashboardProps } from './icons/menus/dashboard/IconMenuDashboard';
+export { IconMenuHome, type IconMenuHomeProps } from './icons/menus/home/IconMenuHome';
+export { IconMenuIntegration, type IconMenuIntegrationProps } from './icons/menus/integration/IconMenuIntegration';
+export { IconMenuLog, type IconMenuLogProps } from './icons/menus/log/IconMenuLog';
+export { IconMenuPlan, type IconMenuPlanProps } from './icons/menus/plan/IconMenuPlan';
+export { IconMenuTemplate, type IconMenuTemplateProps } from './icons/menus/template/IconMenuTemplate';
+export { IconMenuWorkflow, type IconMenuWorkflowProps } from './icons/menus/workflow/IconMenuWorkflow';
