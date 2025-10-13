2d3dbba048f13b27ccd76ffb81ffd4d6a2f1a068
xing_lin <xing_lin@fzzixun.com>
2025-10-13 17:14:23 +0800

    chore: export root.css

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
 import postcss from 'rollup-plugin-postcss';
 
 const packageJson = require('./package.json');
@@ -30,7 +29,11 @@ export default [
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       terser(),
-      postcss(),
+      postcss({
+        extract: 'root.css',
+        minimize: true,
+        sourceMap: true,
+      }),
     ],
     external: ['react', 'react-dom'],
   },
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+import './root.css';
+
 export * from './components';
