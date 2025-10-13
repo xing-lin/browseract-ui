351c8a184857063ee0610a7dcaa6407741287dda
xing_lin <xing_lin@fzzixun.com>
2025-10-13 22:23:52 +0800

    fix: validate filename

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,7 +7,7 @@ export { IconMenuAgent, type IconMenuAgentProps } from './icons/menus/agent/Icon
 export { IconMenuCredential, type IconMenuCredentialProps } from './icons/menus/credential/IconMenuCredential';
 export { IconMenuDashboard, type IconMenuDashboardProps } from './icons/menus/dashboard/IconMenuDashboard';
 export { IconMenuHome, type IconMenuHomeProps } from './icons/menus/home/IconMenuHome';
-export { IconMenuIntegration, type IconMenuIntegrationProps } from './icons/menus/integration1/IconMenuIntegration';
+export { IconMenuIntegration, type IconMenuIntegrationProps } from './icons/menus/integration/IconMenuIntegration';
 export { IconMenuLog, type IconMenuLogProps } from './icons/menus/log/IconMenuLog';
 export { IconMenuPlan, type IconMenuPlanProps } from './icons/menus/plan/IconMenuPlan';
 export { IconMenuTemplate, type IconMenuTemplateProps } from './icons/menus/template/IconMenuTemplate';
