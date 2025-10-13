98c5d8f7a12ed08d595be6f9b03f6937861bc5bd
xing_lin <xing_lin@fzzixun.com>
2025-10-13 20:58:08 +0800

    feat: add menu plan icon

---
+++ src/components/icons/menus/plan/IconMenuPlan.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { IconMenuPlan } from './IconMenuPlan';
+
+const meta: Meta<typeof IconMenuPlan> = {
+  title: 'Icons/menus/IconMenuPlan',
+  component: IconMenuPlan,
+  argTypes: {
+    color: {
+      control: 'color',
+    },
+    active: {
+      control: 'boolean',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof IconMenuPlan>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Active: Story = {
+  args: {
+    active: true,
+  },
+};
+
+export const Hover: Story = {
+  args: {},
+  render: (args) => (
+    <button className="icon-btn-hover">
+      <IconMenuPlan {...args} />
+    </button>
+  ),
+};
