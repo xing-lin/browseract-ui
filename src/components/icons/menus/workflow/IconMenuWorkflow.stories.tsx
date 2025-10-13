e701db543a96eddd2d2e42c12c65039de314e96d
xing_lin <xing_lin@fzzixun.com>
2025-10-13 19:27:55 +0800

    feat: add menu workflow icon

---
+++ src/components/icons/menus/workflow/IconMenuWorkflow.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { IconMenuWorkflow } from './IconMenuWorkflow';
+
+const meta: Meta<typeof IconMenuWorkflow> = {
+  title: 'Icons/menus/IconMenuWorkflow',
+  component: IconMenuWorkflow,
+  argTypes: {
+    color: {
+      control: 'color',
+    },
+    active: {
+      control: 'boolean',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof IconMenuWorkflow>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Active: Story = {
+  args: {
+    active: true,
+  },
+};
+
+export const Hover: Story = {
+  args: {},
+  render: (args) => (
+    <button className="icon-btn-hover">
+      <IconMenuWorkflow {...args} />
+    </button>
+  ),
+};
