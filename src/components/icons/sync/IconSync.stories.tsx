f5d8f4ad7d7f799a2784488db39431b30779eb4c
xing_lin <xing_lin@fzzixun.com>
2025-10-16 15:57:32 +0800

    feat: 添加 sync 图标

---
+++ src/components/icons/sync/IconSync.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { IconSync } from './IconSync';
+import { commonIconArgTypes } from '../constants';
+
+const meta: Meta<typeof IconSync> = {
+  title: 'Icons/IconSync',
+  component: IconSync,
+  argTypes: commonIconArgTypes,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof IconSync>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Loading: Story = {
+  args: {},
+  render: (args) => (
+    <IconSync
+      {...args}
+      className="_spinner"
+    />
+  ),
+};
