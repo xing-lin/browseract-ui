809c243d53ba3fb65bcb16458ba3df05105489c8
xing_lin <xing_lin@fzzixun.com>
2025-10-16 15:57:22 +0800

    feat: 添加 setting 图标

---
+++ src/components/icons/setting/IconSetting.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { IconSetting } from './IconSetting';
+import { commonIconArgTypes } from '../constants';
+
+const meta: Meta<typeof IconSetting> = {
+  title: 'Icons/IconSetting',
+  component: IconSetting,
+  argTypes: commonIconArgTypes,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof IconSetting>;
+
+export const Default: Story = {
+  args: {},
+};
