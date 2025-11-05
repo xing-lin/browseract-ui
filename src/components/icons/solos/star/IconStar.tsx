572ce6440566bd39e3a0d23481b7a1f7c0239a96
xing_lin <xing_lin@fzzixun.com>
2025-11-05 17:30:38 +0800

    fix: update IconStar viewBox

--- src/components/icons/solos/star/IconStar.tsx
+++ src/components/icons/solos/star/IconStar.tsx
@@ -4,7 +4,7 @@ export function IconStar(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...baseIconConfigs}
-      viewBox="0 0 16 16"
+      viewBox="0 0 9 9"
       {...props}
     >
       <path d="m0 4.5 2.424.864a2 2 0 0 1 1.212 1.212L4.5 9l.864-2.424a2 2 0 0 1 1.212-1.212L9 4.5l-2.424-.864a2 2 0 0 1-1.212-1.212L4.5 0l-.864 2.424a2 2 0 0 1-1.212 1.212z" />
