7f386d78844154322b7cd6cd5b504830172e22b3
xing_lin <xing_lin@fzzixun.com>
2025-11-05 18:23:14 +0800

    feat: tabs 添加card跟 underline

--- src/components/tabs/Tabs.stories.tsx
+++ src/components/tabs/Tabs.stories.tsx
@@ -3,6 +3,9 @@ import { Tabs } from './Tabs';
 import { IconChain } from '../icons/solos/chain/IconChain';
 import { IconDuoLog } from '../icons/duos/log/IconDuoLog';
 import { IconToolPreview } from '../icons/solos/tool/IconToolPreview';
+import { IconQuestionCircle } from '../icons/solos/question/IconQuestionCircle';
+import { IconApiAutomation } from '../icons/solos/api/IconApiAutomation';
+import { IconMcp } from '../icons/solos/mcp/IconMcp';
 
 const meta: Meta<typeof Tabs> = {
   title: 'Components/Tabs',
@@ -11,7 +14,7 @@ const meta: Meta<typeof Tabs> = {
   argTypes: {
     type: {
       control: 'select',
-      options: ['default', 'primitive'],
+      options: ['default', 'primitive', 'card', 'underline'],
     },
   },
 };
@@ -54,5 +57,62 @@ export const Default: Story = {
       },
     ],
     type: 'default',
+  },
+};
+
+export const Underline: Story = {
+  args: {
+    options: [
+      {
+        label: (
+          <>
+            Exposed
+            <span className="text-(--color-text-tertiary)">(3)</span>
+            <IconQuestionCircle className="text-(--color-text-tertiary)" />
+          </>
+        ),
+        value: 'tab1',
+        children: 'Tab 1',
+      },
+      {
+        label: (
+          <>
+            Not Exposed
+            <IconQuestionCircle className="text-(--color-text-tertiary)" />
+          </>
+        ),
+        value: 'tab2',
+        children: 'Tab 2',
+      },
+    ],
+    type: 'underline',
+  },
+};
+
+export const Card: Story = {
+  args: {
+    options: [
+      {
+        label: (
+          <>
+            <IconApiAutomation />
+            API & Automation
+          </>
+        ),
+        value: 'tab1',
+        children: 'Tab 1',
+      },
+      {
+        label: (
+          <>
+            <IconMcp />
+            Connect to MCP Clients
+          </>
+        ),
+        value: 'tab2',
+        children: 'Tab 2',
+      },
+    ],
+    type: 'card',
   },
 };
--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -11,9 +11,11 @@ export interface TabsProps {
     label: React.ReactNode;
     value: string;
     children: React.ReactNode;
+    childrenClassName?: string;
+    labelClassName?: string;
   }[];
   className?: string;
-  type?: 'default' | 'primitive';
+  type?: 'default' | 'primitive' | 'card' | 'underline';
   value?: string;
   onValueChange?: (value: string) => void;
 }
@@ -34,34 +36,67 @@ export function Tabs({
       defaultValue={options[0].value}
       value={value}
       onValueChange={onValueChange}
-      className={className}
+      className={cn(
+        'gap-0',
+        {
+          'mr-auto': type === 'underline',
+        },
+        className,
+      )}
     >
       <TabListUI
-        className={cn({
+        className={cn('w-full', {
           'bg-[#F5F5F5]': type === 'default',
+          'justify-start gap-6 rounded-none border-b border-(--color-border) bg-transparent p-0':
+            type === 'underline',
+          'justify-start gap-0.5 rounded-none border-b border-(--color-border) bg-transparent px-6 py-0':
+            type === 'card',
         })}
       >
-        {options.map((option) => (
-          <TabsTriggerUI
-            key={option.value}
-            value={option.value}
-            className={cn({
-              'text-(--color-text-secondary) data-[state=active]:bg-white data-[state=active]:text-(--color-text) data-[state=active]:shadow-[0_1px_2px_0_rgba(0,0,0,0.06),0_1px_3px_0_rgba(0,0,0,0.1)]':
-                type === 'default',
-            })}
-          >
-            {option.label}
-          </TabsTriggerUI>
-        ))}
+        {options.map((option) => {
+          const { value, label, labelClassName } = option;
+          return (
+            <TabsTriggerUI
+              key={value}
+              value={value}
+              className={cn(
+                'font-normal text-(--color-text-secondary) data-[state=active]:font-medium data-[state=active]:text-(--color-text)',
+                {
+                  'data-[state=active]:bg-white data-[state=active]:shadow-[0_1px_2px_0_rgba(0,0,0,0.06),0_1px_3px_0_rgba(0,0,0,0.1)]':
+                    type === 'default',
+                  'h-[calc(100%+2px)] flex-none rounded-none border-0 border-b-2 border-b-transparent bg-transparent px-1 shadow-none data-[state=active]:border-b-(--tabs-underline-border-active) data-[state=active]:shadow-none':
+                    type === 'underline',
+                },
+                {
+                  'h-[calc(100%+2px)] flex-none rounded-none rounded-t-md border border-(--color-border) bg-(--tabs-card-bg) px-4 shadow-none data-[state=active]:border-b-(--tabs-card-bg-active) data-[state=active]:bg-(--tabs-card-bg-active) data-[state=active]:font-normal data-[state=active]:text-(--tabs-card-text-active) data-[state=active]:shadow-none':
+                    type === 'card',
+                },
+                labelClassName,
+              )}
+            >
+              {label}
+            </TabsTriggerUI>
+          );
+        })}
       </TabListUI>
-      {options.map((option) => (
-        <TabsContentUI
-          key={option.value}
-          value={option.value}
-        >
-          {option.children}
-        </TabsContentUI>
-      ))}
+      {options.map((option) => {
+        const { value, children, childrenClassName } = option;
+        return (
+          <TabsContentUI
+            key={value}
+            value={value}
+            className={cn(
+              'pt-6',
+              {
+                'p-6': type === 'card',
+              },
+              childrenClassName,
+            )}
+          >
+            {children}
+          </TabsContentUI>
+        );
+      })}
     </TabsUI>
   );
 }
